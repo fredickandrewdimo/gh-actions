c5bddddacf954c2f4bac13939515a74c3f1eba95
Andrew <fredickandrewdimo@gmail.com>
2023-09-30 11:43:38 +0800

    add scenario for fail the test.

--- __test__/hello.test.js
+++ __test__/hello.test.js
@@ -1,5 +1,7 @@
 const { greet } = require("../hello");
 
 test("greet function", () => {
-  expect(greet("Github Actions!")).toBe("Hello, Github Actions!");
+  expect(greet("Github Actions!")).toBe(
+    "Hello, Github Actions! For Failing the Test!"
+  );
 });
